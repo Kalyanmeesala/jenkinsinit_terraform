fe4d64242a88a034b515e01cbd8195b767d90a0c
Kalyan Meesala <kalyanmeesala@Kalyans-MBP.lan>
2020-08-02 12:02:02 -0500

    adding files to github

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "testingfrom-jenkins"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Use AWS Terraform provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "jenkins-terraform-intigration"
+  acl    = "private"
+
+  tags = {
+    Name        = "jenkins-terraform-intigration"
+    Environment = "Dev"
+  }
+}
